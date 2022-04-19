bc342e10e39b7a0dcdbe9fc150d74f7b001ebc25
octopunkk <anais.derue@insa-rennes.fr>
2022-04-19 21:03:19 +0200

    rm classes + add init method + ex json lvl file

--- sokoban.py
+++ sokoban.py
@@ -1,74 +1,44 @@
 import pygame   
+import json 
 
-pygame.init()  
-window = pygame.display.set_mode((600, 600)) 
-pygame.display.set_caption("Sokoban") 
+
+# pygame.init()  
+# window = pygame.display.set_mode((600, 600)) 
+# pygame.display.set_caption("Sokoban") 
 
 run = True
-
-class Player:
-  def __init__(self):
-    self.x = 200
-    self.y = 200
-    self.width = 40
-    self.height = 40
-    self.vel = 40
-    self.hasMoved = False
-
-class Game(Player):
-
-  def __init__(self):
-    super(Player, self).__init__()
-    self.frame = 0
-    self.wallCoordinates = []
-
-  def init_pygame(self):
-    pygame.init()
-    pygame.font.init()
-
-    
-  def drawWall(self,x,y):
-    self.wallCoordinates.append([x,y])
-    width = 40
-    height = 40
-    pygame.draw.rect(window, (0, 255, 0), (x, y, width, height))  
-
-  def noWallCollision(self, x, y):
-    noCollision = True
-    for wall in self.wallCoordinates :
-      if wall[0] == x and wall[1] == y :
-        noCollision = False
-    return noCollision
+with open('level0.json', 'r') as f:
+  lvl0 = json.load(f)
+# Lvl 0 :
+    # # # # #
+    #   x   #
+    # #     #
+    # * @   #
+    # # # # #
+# Player = x
+# Box = @
+# Goal = *
+# Wall = #
 
 
-game = Game()
-player = Player()
-game.init_pygame()
+def initGrid(level):
+  player = level["player"]
+  walls = level["walls"]
+  boxes = level["boxes"]
+  goals = level["goals"]
+  size = level["size"]
+  grid = [0] * size * size
+  for wall in walls:
+    grid[wall[0] * size + wall[1]] = "#"
+  for box in boxes:
+    grid[box[0] * size + box[1]] = "@"
+  for goal in goals:
+    grid[goal[0] * size + goal[1]] = "*"
+  grid[player[0] * size + player[1]] = "x"
+  return grid
 
-while run: 
-    pygame.time.delay(20) 
-    player.hasMoved = False
-    for event in pygame.event.get(): 
-        if event.type == pygame.QUIT: 
-            run = False
-    keys = pygame.key.get_pressed() 
-    if keys[pygame.K_LEFT] and player.x>0 and game.noWallCollision(player.x-player.vel, player.y): 
-        player.x -= player.vel
-        player.hasMoved = True
-    if keys[pygame.K_RIGHT] and player.x<600-player.width and game.noWallCollision(player.x+player.vel, player.y): 
-        player.x += player.vel 
-        player.hasMoved = True
-    if keys[pygame.K_UP] and player.y>0 and game.noWallCollision(player.x, player.y-player.vel): 
-        player.y -= player.vel 
-        player.hasMoved = True
-    if keys[pygame.K_DOWN] and player.y<600-player.height and game.noWallCollision(player.x, player.y+player.vel): 
-        player.y += player.vel 
-        player.hasMoved = True
-    window.fill((0, 0, 0)) 
-    pygame.draw.rect(window, (255, 0, 0), (player.x, player.y, player.width, player.height)) 
-    game.drawWall(0,0)
-    pygame.display.update()  
-    if(player.hasMoved):
-      pygame.time.delay(200)
-pygame.quit() 
+initGrid(lvl0)
+# while run: 
+#     ## do stuff
+# pygame.quit() 
 
